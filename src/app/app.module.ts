ccebdf0e3381b0ad29ae44166cd947873bae7dde
Marco Berardini <mberardini@evolution-man.com>
2016-12-04 20:40:53 -0800

    [client] Modulerized ShoppingList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from "./app.routes";
+
+import { DropdownDirective } from './dropdown.directive';
+
+import { RecipeService } from "./recipes/recipe.service";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -9,14 +15,9 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
-import { DropdownDirective } from './dropdown.directive';
-import { RecipeService } from "./recipes/recipe.service";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import { routing } from "./app.routes";
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start.component';
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 
 @NgModule({
   declarations: [
@@ -26,18 +27,15 @@ import { RecipeStartComponent } from './recipes/recipe-start.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
     DropdownDirective,
     RecipeEditComponent,
-    RecipeStartComponent,
     RecipeStartComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     ReactiveFormsModule,
+    ShoppingListModule,
     routing
   ],
   providers: [RecipeService, ShoppingListService],
